2b948e1ef507fe03f870f1a0661d9488e0ffe110
Nathaniel Alcedo <nate@natealcedo.dev>
2024-10-05 23:47:05 +0800

    Use header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+
+export const routes: Routes = [
+  {
+    path: 'sign-in',
+    component: SignInComponent,
+  },
+];
